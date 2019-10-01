ed9ddafb0d620168ea8e3ab4884f3457b8525c68
Mike Smith <grimbough@gmail.com>
2019-10-01 11:00:18 +0200

    Fixed some examples reference missing entrezgene attribute

--- tests/testthat/test_getBM.R
+++ tests/testthat/test_getBM.R
@@ -12,13 +12,5 @@ test_that("Fail when no dataset is specified", {
      ensembl=useMart("ensembl")
      expect_error(getBM(mart = ensembl), "No dataset selected, please select a dataset first")
 })
-# 
-# ## no attributes are specified')
-# ensembl = useDataset("hsapiens_gene_ensembl",mart=ensembl)
-# expect_error(getBM(mart = ensembl), "Argument 'attributes' must be specified")
-# 
-# t1 <- getBM(attributes='entrezgene', filters = 'affy_hg_u133_plus_2', values = '207500_at', mart = ensembl)[1,1]
-# expect_equal(as.integer(t1), 838)
-# 
-# 
+
  
